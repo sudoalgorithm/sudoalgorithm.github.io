45e1d7eb28b03dff9bec9d280d368fca8b53df58
Kunal Malhotra <KunalMalhotra@abcs-MacBook-Air-2.local>
2019-12-30 23:33:41 +0400

    added github-calendar.js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,6 @@ particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
+GitHubCalendar(".calendar", "sudoalgorithm");
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "sudoalgorithm", { responsive: true });
