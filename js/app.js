63dbf36de9d5a0c46076e2475c596cd357c92c6d
Kunal Malhotra <me@knrmalhotra.com>
2023-09-16 10:45:56 +0200

    Fixed the github calender code base

--- js/app.js
+++ js/app.js
@@ -2,6 +2,4 @@ particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
-GitHubCalendar(".calendar", "KunalMalhotraN26");
-// or enable responsive functionality:
-GitHubCalendar(".calendar", "KunalMalhotraN26", { responsive: true });
+GitHubCalendar(".calendar", "sudoalgorithm", { responsive: true });
